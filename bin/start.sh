5c626c25137df9702932712062b6aa876b0ab7d7
Thoriq Firdaus <thoriqoe@gmail.com>
2018-07-15 12:28:34 +0700

    User exec instead of run
    Use exec since the container is already up and running at this stage.

--- bin/start.sh
+++ bin/start.sh
@@ -8,7 +8,7 @@ export WP_LOCKER_START=1
 
 "$(dirname "$0")"/up.sh --build -d
 
-while ! drun cat wp-config.php > /dev/null 2>&1; do
+while ! dexec cat wp-config.php > /dev/null 2>&1; do
 	sleep 2
 done
 
